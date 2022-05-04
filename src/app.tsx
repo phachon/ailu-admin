627e2e9d458e9cdd90930fa7993a88b56991d7e4
phachon <phachon@163.com>
2022-05-04 15:49:32 +0800

    [feat] add antd

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Button} from "antd";
+import './app.css';
 
 class App extends React.Component<any, any> {
 
     render() {
         return (
             <div className="App">
-                hello world
+                <Button type="primary">Button</Button>
             </div>
         );
     }
