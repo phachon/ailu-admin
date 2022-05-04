f5a89cb0a28d7d47c90e6ee799b5a83445b9e244
phachon <phachon@163.com>
2022-05-04 15:32:38 +0800

    [feat] hello word

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component<any, any> {
+
+    render() {
+        return (
+            <div className="App">
+                hello world
+            </div>
+        );
+    }
+}
+
+export default App;
