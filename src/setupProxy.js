d9f366b1a394604edf4e5acd0dfeb388eed26185
phachon <phachon@163.com>
2023-01-19 16:51:10 +0800

    feat: upgrade antd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
     app.use(
         createProxyMiddleware("/admin-api", {
             // target: "http://127.0.0.1:8088/",
-            target: "http://www.dingxn.cn:28088/",
             changeOrigin: true,
             pathRewrite: {
                 "^/admin-api": ""
